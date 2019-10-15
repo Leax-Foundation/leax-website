227edb3133127d3f7432a25ccb681267537d71cf
dkdaniz <daniel@descentralizado.com.br>
2019-10-15 14:19:46 -0300

    .

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const sessionService = require("./services/sesssion.service");
 const controllers = require("./controllers/index");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const analytics = expressGoogleAnalytics("UA-122948014-1");
 app.use(analytics);
